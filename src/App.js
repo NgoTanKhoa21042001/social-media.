cca953b72144e83ce84bd12a1e75d8c042a81f7d
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-01-13 23:16:55 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PageRender from "./PageRender";
+import Login from "./pages/login/Login.jsx";
 
 function App() {
   return (
-    <Router>
-      <input type="checkbox" id="theme" />
-      <div className="App">
-        <div className="main">
-          {" "}
-          <Routes>
-            <Route exact path="/:page" element={<PageRender />} />
-            <Route exact path="/:page/:id" element={<PageRender />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div>
+      <Login />
+    </div>
   );
 }
 

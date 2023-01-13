432e50c0b133cfd88b9166e70f9cc3fc93668b59
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-01-13 23:20:02 +0700

    edit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./login.scss";
 const Login = () => {
-  return <div className="login">Login</div>;
+  return <div className="login">ddfdf</div>;
 };
 
 export default Login;
